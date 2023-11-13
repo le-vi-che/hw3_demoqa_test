b41e9a1a906b956c3391517ec6ed126ddd68707d
emonovaev <lena.jemonova@gmail.com>
2023-11-13 17:57:15 +0300

    add class with faker

--- src/test/java/tests/PracticeFormWithFaker.java
+++ src/test/java/tests/PracticeFormWithFaker.java
@@ -3,7 +3,7 @@ package tests;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
-public class PracticeFormWithPageObject extends TestBase {
+public class PracticeFormWithFaker extends TestBase {
 
   RegistrationPage registrationPage = new RegistrationPage();
 
